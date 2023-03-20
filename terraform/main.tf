92fe92dd7e235f769b0835a0d27c2c76763ff9b4
sstoneju <sstoneju@kakao.com>
2023-03-20 23:06:14 +0900

    chore

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,17 +41,17 @@ provider "aws" {
   }
 }
 
-provider "kubernetes" {
-  host = module.eks_bottlerocket.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks_bottlerocket.cluster_certificate_authority_data)
+# provider "kubernetes" {
+#   host = module.eks_bottlerocket.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.eks_bottlerocket.cluster_certificate_authority_data)
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks_bottlerocket.cluster_id]
-  }
-}
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1alpha1"
+#     command     = "aws"
+#     # This requires the awscli to be installed locally where Terraform is executed
+#     args = ["eks", "get-token", "--cluster-name", module.eks_bottlerocket.cluster_id]
+#   }
+# }
 
 module "tags" {
   source  = "clowdhaus/tags/aws"
